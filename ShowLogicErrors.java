4776c284bb5b8e1931c701cd33845a12a6fa9a4f
Jacob Stevenson <Jacobryanstevenson@gmail.com>
2021-12-08 16:03:33 -0600

    Add files via upload

---
+++ ShowLogicErrors.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ar114
+ */
+public class ShowLogicErrors 
+{
+    
+   public static void main(String[] args) 
+   {
+    System.out.println("Celsius 35 is Fahrenheit degree ");
+    System.out.println((9 / 5.0) * 35 + 32);
+  }
+}
